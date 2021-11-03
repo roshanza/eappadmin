ce5ac9ac88b39caf50a4246a9880c3e7ce52a97c
roshanza <roshanz@gmail.com>
2021-11-03 16:12:39 +1100

    Update IndustryController.java

--- src/main/java/au/com/metlife/reference/controller/IndustryController.java
+++ src/main/java/au/com/metlife/reference/controller/IndustryController.java
@@ -17,9 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import au.com.metlife.reference.dto.IndustryAdminDTO;
-import au.com.metlife.reference.dto.IndustryDTO;
-import au.com.metlife.reference.service.IndustryService;
+
 import au.com.metlife.reference.utils.CommonUtils;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -32,9 +30,6 @@ public class IndustryController {
 	private static final Logger LOGGER=LoggerFactory.getLogger(IndustryController.class);
 	
 
-	@Autowired 
-	IndustryService  indService;
-	
 	
 	@ApiOperation(value = "", authorizations = { @Authorization(value="jwtToken") })
 	@GetMapping(value ="/secure/industry",produces = "application/vnd.aumet.app-v1+json")
@@ -43,32 +38,10 @@ public class IndustryController {
 				
 		String fund=CommonUtils.getFundCodeFromJWT(jwt);		
 		List<IndustryDTO> industry=indService.getIndustries(fund);
-		return new ResponseEntity<List<IndustryDTO>>(industry,HttpStatus.OK);	
+		return new ResponseEntity<String>(fund,HttpStatus.OK);	
 	}
 	
 	
-	
-	@ApiOperation(value = "", authorizations = { @Authorization(value="jwtToken") })
-	@GetMapping(value ="/secure/industry",produces = "application/vnd.aumet.app-v2+json")
-	
-	public  ResponseEntity <?>getIndustryV2(@ApiParam(hidden = true) @AuthenticationPrincipal Jwt jwt) {		
-				
-		String fund=CommonUtils.getFundCodeFromJWT(jwt);		
-		List<IndustryDTO> industry=indService.getIndustries(fund);
-		return new ResponseEntity<List<IndustryDTO>>(industry,HttpStatus.OK);	
-	}
-	
-	
-	
-	
-	  @ApiOperation(value = "", authorizations = { @Authorization(value="jwtToken")	  })	  
-	  @PostMapping(value ="/admin/industry",produces =	  "application/vnd.aumet.app-v1+json")	  
-	  public IndustryAdminDTO createIndustry(@ApiParam(hidden =true) @AuthenticationPrincipal Jwt jwt,@Valid @RequestBody IndustryAdminDTO indDTO) { 
-	  
-	  	return indService.createIndustry(indDTO);
-	  
-	  }
-	 
 	 
 
 }
