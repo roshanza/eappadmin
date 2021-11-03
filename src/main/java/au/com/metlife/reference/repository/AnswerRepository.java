8f5b6f3d8317da2f27f18faf69412cce5641649f
roshanza <roshanz@gmail.com>
2021-11-03 16:10:53 +1100

    Delete src/main/java/au/com/metlife/reference/repository directory

--- src/main/java/au/com/metlife/reference/repository/AnswerRepository.java
+++
@@ -1,9 +0,0 @@
-package au.com.metlife.reference.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import au.com.metlife.reference.entity.Answer;
-
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-	
-}
